daaa499e725112cac1c7c918e8bf2eaef6207d77
Jessica Strömberg <jessica.stromberg@live.se>
2017-05-16 21:29:08 +0200

    calendar styling

--- public/js/script.js
+++ public/js/script.js
@@ -3,24 +3,17 @@ $(document).ready(function(){
   $('#signUpModal').modal('open');
   $('#loginModal').modal('open');
 
-  $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year
-    format: 'dd-mm-yyyy',
-    firstDay: 'Monday',
-  });
-
-  $('input.autocomplete').autocomplete({
-    data: {
-      "Apple": null,
-      "Microsoft": null,
-      "Google": 'http://placehold.it/250x250'
-    },
-    limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
-    onAutocomplete: function(val) {
-      // Callback function when value is autcompleted.
-    },
-    minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
-  });
+  $('#calendar').fullCalendar({
+      // locale: 'sv'
+      weekNumbers: true,
+      height: 720,
+      fixedWeekCount: false,
+      header: {
+        left:   'today prev,next',
+        center: 'title',
+        right:  'month,basicWeek,agendaDay '
+      },
+      columnFormat: 'dddd'
+    });
 
 });
